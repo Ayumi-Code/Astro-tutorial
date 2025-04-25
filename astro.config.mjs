7538e5b9983471a9c86c2a4fa39ec2780a16d365
HAMANO Ayumi <ayumi@flag-a.com>
2025-04-25 11:15:52 +0900

    不要ファイルを削除、等

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://creator-ayu.netlify.app"
+  site: "https://creator-ayu.netlify.app",
+  // base: '/my_astro/',
+  integrations: [preact()]
 });
